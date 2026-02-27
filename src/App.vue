110414b4036ea67b80f32539bb77813f3f48a6a8
Your Name <you@example.com>
2026-02-27 11:05:00 +0500

    1

--- src/App.vue
+++ src/App.vue
@@ -5,14 +5,14 @@ const contacts = ref([
   {
     id: 'instagram',
     name: 'Instagram',
-    value: '@bobojonov_treder',
+    value: '@_bobojonovv._.b',
     link: 'https://instagram.com/_bobojonovv._.b',
     icon: `<svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><rect width="20" height="20" x="2" y="2" rx="5" ry="5"/><path d="M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z"/><line x1="17.5" x2="17.51" y1="6.5" y2="6.5"/></svg>`,
   },
   {
     id: 'telegram',
     name: 'Telegram',
-    value: '@@Boburjon_fx',
+    value: '@Boburjon_fx',
     link: 'https://t.me/@Boburjon_fx',
     icon: `<svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="m22 2-7 20-4-9-9-4Z"/><path d="M22 2 11 13"/></svg>`,
   },
